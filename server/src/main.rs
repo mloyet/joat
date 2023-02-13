b551512921a6b392c67f4c2421bb55ebaba14f0b
Mason Loyet <mloyet@andrew.cmu.edu>
2023-02-13 10:42:20 -0500

    Write echoer code for server side

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+
+
+fn echoer(mut s: TcpStream) -> std::io::Result<()> {
+  let mut buf: [u8;1024] = [0;1024];
+  loop {
+    let amt = s.read(&mut buf)?;
+    s.write(&buf[0..amt])?;
+  }
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+
+    for stream in listener.incoming() {
+      echoer(stream?)?;
+    }
+
+    Ok(())
 }
