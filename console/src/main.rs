083cb52a219e8377ea6b2dc07ba8a0e2c420b87e
Mason Loyet <mloyet@andrew.cmu.edu>
2023-04-05 09:28:39 -0400

    Make server wait

--- console/src/main.rs
+++ console/src/main.rs
@@ -4,7 +4,7 @@
 
 use std::{
   io::{self, BufRead},
-  net::TcpStream,
+  net::TcpListener,
 };
 
 use protocol::{Card, Message, Protocol, Rank, Suit};
@@ -17,79 +17,85 @@ const HELP: &str = "Commands:
     print <msg>                 Print a message to the LCD screen.";
 
 fn main() -> io::Result<()> {
-  println!("[console] Creating protocol");
-  let mut prot = Protocol::new(TcpStream::connect("127.0.0.1:8000").unwrap());
-  println!("[console] done.");
+  let listener = TcpListener::bind("0.0.0.0:8000")?;
 
-  let mut line = String::new();
-  let stdin = io::stdin();
-  let mut handle = stdin.lock();
-  loop {
-    print!("> ");
-    use Message::*;
-    handle.read_line(&mut line)?;
+  for stream in listener.incoming() {
+    let stream = stream?;
+    println!("Attached to client {:?}", stream);
 
-    // check for the single word commands.
-    if line == "clear" {
-      prot.send_msg(Clear).unwrap();
-      continue;
-    }
-    if line == "scan" {
-      prot.send_msg(RequestScan).unwrap();
-      continue;
-    }
-    if line == "read" {
-      prot.send_msg(ReadInput).unwrap();
-      continue;
-    }
-    if line == "help" {
-      println!("{}", HELP);
-    }
+    let mut prot = Protocol::new(stream);
 
-    // check for commands with arguments.
-    if let Some((cmd, arg)) = line.split_once(" ") {
-      if cmd == "print_card" {
-        if let Some((card, suit)) = arg.split_once(" ") {
-          use Rank::*;
-          use Suit::*;
-          let rank = match card {
-            "A" => ACE,
-            "2" => TWO,
-            "3" => THREE,
-            "4" => FOUR,
-            "5" => FIVE,
-            "6" => SIX,
-            "7" => SEVEN,
-            "8" => EIGHT,
-            "9" => NINE,
-            "10" => TEN,
-            "J" => JACK,
-            "Q" => QUEEN,
-            "K" => KING,
-            _ => {
-              println!("Enter valid card: 1-10JQKA.");
-              continue;
-            }
-          };
-          let suit = match suit {
-            "H" => HEART,
-            "D" => DIAMOND,
-            "S" => SPADE,
-            "C" => CLUB,
-            _ => {
-              println!("Enter valid suit: HDSC.");
-              continue;
-            }
-          };
-          prot.send_msg(PrintCard(Card(suit, rank))).unwrap();
-        }
-      } else if cmd == "print" {
-        prot.send_msg(Print(arg.to_string())).unwrap();
+    let mut line = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    loop {
+      print!("> ");
+      use Message::*;
+      handle.read_line(&mut line)?;
+
+      // check for the single word commands.
+      if line == "clear" {
+        prot.send_msg(Clear).unwrap();
         continue;
       }
-    }
+      if line == "scan" {
+        prot.send_msg(RequestScan).unwrap();
+        continue;
+      }
+      if line == "read" {
+        prot.send_msg(ReadInput).unwrap();
+        continue;
+      }
+      if line == "help" {
+        println!("{}", HELP);
+      }
 
-    println!("Unrecognized command: {}", line);
-    println!("{}", HELP);
+      // check for commands with arguments.
+      if let Some((cmd, arg)) = line.split_once(" ") {
+        if cmd == "print_card" {
+          if let Some((card, suit)) = arg.split_once(" ") {
+            use Rank::*;
+            use Suit::*;
+            let rank = match card {
+              "A" => ACE,
+              "2" => TWO,
+              "3" => THREE,
+              "4" => FOUR,
+              "5" => FIVE,
+              "6" => SIX,
+              "7" => SEVEN,
+              "8" => EIGHT,
+              "9" => NINE,
+              "10" => TEN,
+              "J" => JACK,
+              "Q" => QUEEN,
+              "K" => KING,
+              _ => {
+                println!("Enter valid card: 1-10JQKA.");
+                continue;
+              }
+            };
+            let suit = match suit {
+              "H" => HEART,
+              "D" => DIAMOND,
+              "S" => SPADE,
+              "C" => CLUB,
+              _ => {
+                println!("Enter valid suit: HDSC.");
+                continue;
+              }
+            };
+            prot.send_msg(PrintCard(Card(suit, rank))).unwrap();
+          }
+        } else if cmd == "print" {
+          prot.send_msg(Print(arg.to_string())).unwrap();
+          continue;
+        }
+      }
+
+      println!("Unrecognized command: {}", line);
+      println!("{}", HELP);
+    }
   }
+  Ok(())
 }
