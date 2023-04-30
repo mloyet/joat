a7258ca7c75a82e810546d096a6ad6395386110c
Mason Loyet <mloyet@andrew.cmu.edu>
2023-04-30 12:54:38 -0400

    Add model

---
+++ model/detection.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+model = YOLO('best.pt')  # load a custom model
+while True:
+    image = input()
+    results = model(image, verbose = False)
+    for prediction in results[0].boxes.cls:
+        print(results[0].names[prediction.item()])
+    print('.')
