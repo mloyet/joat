35adde71de6c1af8568269dcd4e9340f34fe19da
Mason Loyet <mloyet@andrew.cmu.edu>
2023-05-07 21:36:38 -0400

    Fix bitmaps

--- device/src/script.py
+++ device/src/script.py
@@ -1,7 +1,7 @@
 import sys
 import binascii
 
-data = sys.stdin.read().replace('\r\n', ' ').replace('0x','').split(', ')[:-1]
+data = sys.stdin.read().replace('\r\n', ' ').replace('\n', ' ').replace('0x','').replace(' ', '').split(',')
 raw = [binascii.unhexlify(word) for word in data]
 
 for word in raw:
